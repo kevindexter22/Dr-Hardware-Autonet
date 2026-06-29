0f5e33404edd5e9a390cda1dfc504451486f60c9
Kevin Oliveira <rodrigokevin22@outlook.com>
2026-06-29 18:36:27 -0300

    Create ps2_monitor.sh for PS2 IP monitoring
    Added a script to monitor PS2 IP and shutdown if unreachable.

--- 01-infrastructure/storage/opl-smb-storage/ps2_monitor.sh
+++ 01-infrastructure/storage/opl-smb-storage/ps2_monitor.sh
@@ -1,1 +1,21 @@
 
+
+#!/bin/bash
+
+# --- CONFIGURAÇÕES ---
+OPL_PS2_IP="x.x.x.x"  # Substitua pelo IP que deseja monitorar
+TAG="Monitor-Shutdown"   # Tag para filtrar no journalctl
+
+# --- EXECUÇÃO ---
+
+# Realiza 10 tentativas de ping para evitar falso-positivo por oscilação de rede
+if ping -c 10 "$OPL_PS2_IP" > /dev/null 2>&1; then
+    # Se o IP responde, apenas registra no log
+    logger -t "$TAG" "INFO: O PS2 está ligado. Nenhuma ação necessária."
+else
+    # Se o IP NÃO responde, registra o alerta e desliga o servidor
+    logger -t "$TAG" "O PS2 está desligado ou inacessível. Iniciando desligamento do sistema..."
+    
+    # Comando para desligar (requer privilégios de root)
+    /sbin/shutdown -h now
+fi
